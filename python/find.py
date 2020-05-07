2b281d2ca5c56d2a0e29d52bc7eaeb291336ede1
Jorge Facio <facioj@w51lx-facioj.ifw-dresden.de>
2020-05-07 09:00:41 +0200

    several improvements

--- python/find.py
+++ python/find.py
@@ -20,7 +20,7 @@ def from_parser():
 	return my_list
 
 def from_out(max_coor,vacuum):
-	L=max_coor*2+vacuum
+	L=max_coor*2#+vacuum
 	file = open("out",'r')
 	data = file.readlines()
 	file.close()
@@ -43,7 +43,8 @@ def from_out(max_coor,vacuum):
 	return my_list
 
 def identify_blocks():
-	max_coor=113.163
+	#max_coor=113.163
+	max_coor=94.266443463403874
 	vac=10
 	my_list = from_out(max_coor,vac)
 	#my_list2 = from_parser() #this one needs to be changed because of c shift
@@ -68,18 +69,6 @@ def identify_blocks():
 			print block[i][0]
 
 	return my_blocks
-
-
-def process_weights():
-	import pyfplo.common as com
-	wds=com.WeightDefinitions()
-	"example:"
-	w=wds.add(name='all')
-	w.addAtoms(atom='Bi',sites=[1],orbitals=['6p'],fac=1)
-	print "w",w
-
-        bw=com.BandWeights('+bweights')
-        bw.addWeights(wds,'+bwsum')
 
 
 def make_string_def(site,element):
@@ -123,16 +112,243 @@ bwdef simple on
 		"""%locals()
 		return dstring
 
-def print_bwdef(blocks):
+def make_string_def_relativistic(site,element):
 
-	file = open("=.mydef",'w')
+	if(element == "Te" or element=="Bi"):
+
+		if(element == "Te"):
+			n=5
+		else:
+			n=6
+
+		pstring="""
+bwdef simple on
+    contrib
+        site %(site)s
+        orbital %(n)sp3/2+3/2
+        xaxis 1. 0. 0.
+        zaxis 0. 0. 1.
+bwdef simple on
+    contrib
+        site %(site)s
+        orbital %(n)sp3/2-3/2
+        xaxis 1. 0. 0.
+        zaxis 0. 0. 1.
+bwdef simple on
+    contrib
+        site %(site)s
+        orbital %(n)sp3/2+1/2
+        xaxis 1. 0. 0.
+        zaxis 0. 0. 1.
+bwdef simple on
+    contrib
+        site %(site)s
+        orbital %(n)sp3/2-1/2
+        xaxis 1. 0. 0.
+        zaxis 0. 0. 1.
+bwdef simple on
+    contrib
+        site %(site)s
+        orbital %(n)sp1/2+1/2
+        xaxis 1. 0. 0.
+        zaxis 0. 0. 1.
+bwdef simple on
+    contrib
+        site %(site)s
+        orbital %(n)sp1/2-1/2
+        xaxis 1. 0. 0.
+        zaxis 0. 0. 1.
+		"""%locals()
+		return pstring
+	else:
+		dstring="""
+bwdef simple on
+    contrib
+        site %(site)s
+        orbital 3d
+        xaxis 1. 0. 0.
+        zaxis 0. 0. 1.
+		"""%locals()
+		return dstring
+
+def print_bwdef(blocks,rel=False):
+
+	name_file = "=.mydef"
+	if(rel):
+		name_file = "=.mydef_rel"
+	file = open(name_file,'w')
 	print >> file, "coefficients +coeff\n"
 	print >> file, "bweights +bweights_mydef\n"
 
 	for b in blocks:
 		for atom in b:
-			print>> file,make_string_def(atom[0],atom[1])
+			if(rel):
+				print>> file,make_string_def_relativistic(atom[0],atom[1])
+			else:
+				print>> file,make_string_def(atom[0],atom[1])
 	file.close()
 
+def layer_weights(blocks):
+	import pyfplo.common as com
+	wds=com.WeightDefinitions()
+
+	labp = ['p+1up','p+1dn','p+0up','p+0dn','p-1up','p-1dn']
+	labd = ['d+2up','d+2dn','d+1up','d+1dn','d+0up','d+0dn','d-1up','d-1dn','d-2up','d-2dn']
+	b_ind = 0
+	for block in blocks:
+		length = len(block)
+		print length
+                if(length==5):
+			name = """Q_%(b_ind)s"""%locals()
+		else:
+			name = """S_%(b_ind)s"""%locals()
+		w=wds.add(name=name)
+		labels_block = []
+		for atom in block:
+			site_index = atom[0]
+			site = "{:03d}".format(site_index)
+			if(atom[1]=='Bi'):
+				for suffix in labp:
+					label = """Bi(%(site)s)6%(suffix)s"""%locals()
+					labels_block.append(label)
+			if(atom[1]=='Te'):
+				for suffix in labp:
+					label = """Te(%(site)s)5%(suffix)s"""%locals()
+					labels_block.append(label)
+			if(atom[1]=='Mn'):
+				for suffix in labd:
+					label = """Mn(%(site)s)3%(suffix)s"""%locals()
+					labels_block.append(label)
+        	w.addLabels(labels=labels_block,fac=1)
+		print "w",w
+		b_ind += 1
+        bw=com.BandWeights('+bweights_mydef')
+        bw.addWeights(wds,'+bwsum_layer_resolved')
+
+def orbital_layer_weights(blocks):
+	import pyfplo.common as com
+
+	labpx = ['p+1up','p+1dn']
+	labpy = ['p-1up','p-1dn']
+	labpz = ['p+0up','p+0dn']
+	b_ind = 0
+	orbs = [labpx,labpy,labpz]
+	orbs_names = ['px','py','pz']
+	for i in range(3):
+		orb_name = orbs_names[i]
+		labp = orbs[i]
+		wds=com.WeightDefinitions()
+		for block in blocks:
+			length = len(block)
+                	if(length==5):
+				name = """Q_%(b_ind)s"""%locals()
+			else:
+				name = """S_%(b_ind)s"""%locals()
+			w=wds.add(name=name)
+			labels_block = []
+			for atom in block:
+				site_index = atom[0]
+				site = "{:03d}".format(site_index)
+				if(atom[1]=='Bi'):
+					for suffix in labp:
+						label = """Bi(%(site)s)6%(suffix)s"""%locals()
+						labels_block.append(label)
+				if(atom[1]=='Te'):
+					for suffix in labp:
+						label = """Te(%(site)s)5%(suffix)s"""%locals()
+						labels_block.append(label)
+        		w.addLabels(labels=labels_block,fac=1)
+			b_ind += 1
+        	bw=com.BandWeights('+bweights_mydef')
+        	bw.addWeights(wds,"""+bwsum_orb_%(orb_name)s_layer_resolved"""%locals())
+
+def orbital_exp_decay(blocks,lambda_0=20):
+        import pyfplo.common as com
+
+        labpx = ['p+1up','p+1dn']
+        labpy = ['p-1up','p-1dn']
+        labpz = ['p+0up','p+0dn']
+        b_ind = 0
+        orbs = [labpx,labpy,labpz]
+        orbs_names = ['px','py','pz']
+	z_coord_0 = -1000
+	import numpy as np
+	file = open("""weight_lambda_%(lambda_0)s.dat"""%locals(),'w')
+        for i in range(3):
+                orb_name = orbs_names[i]
+                labp = orbs[i]
+                wds=com.WeightDefinitions()
+		ind_atom = 0
+                for block in blocks:
+                        for atom in block:
+                        	labels_atom = []
+                                site_index = atom[0]
+                		w=wds.add(name="""exp_decay_%(orb_name)s_%(site_index)s"""%locals())
+
+				#determine weight
+				z_coord = atom[2][2]
+				if(ind_atom == 0):
+					z_coord_0 = z_coord
+				assert z_coord_0 != -1000
+				rel_z = (z_coord - z_coord_0)/lambda_0
+				weight = np.exp(-rel_z)
+				print >> file, z_coord,rel_z,np.exp(-rel_z)
+
+				#determine label
+                                site = "{:03d}".format(site_index)
+                                if(atom[1]=='Bi'):
+                                        for suffix in labp:
+                                                label = """Bi(%(site)s)6%(suffix)s"""%locals()
+                                                labels_atom.append(label)
+                                if(atom[1]=='Te'):
+                                        for suffix in labp:
+                                                label = """Te(%(site)s)5%(suffix)s"""%locals()
+                                                labels_atom.append(label)
+				ind_atom += 1
+				if(atom[1]!='Mn'):
+                        		w.addLabels(labels=labels_atom,fac=weight)
+                        b_ind += 1
+                bw=com.BandWeights('+bweights_mydef')
+                bw.addWeights(wds,"""+bwsum_orb_%(orb_name)s_lambda_%(lambda_0)s"""%locals())
+	file.close()
+
+def orbital_layer_weights_rel(blocks):
+	import pyfplo.common as com
+
+	labps = ['p3/2+3/2','p3/2-3/2','p3/2+1/2','p3/2-1/2','p1/2+1/2','p1/2-1/2']
+	labps2 = ['p32+32','p32-32','p32+12','p32-12','p12+12','p12-12']
+	b_ind = 0
+	for i in range(6):
+		orb_name = labps[i]
+		wds=com.WeightDefinitions()
+		suffix = orb_name
+		for block in blocks:
+			length = len(block)
+                	if(length==5):
+				name = """Q_%(b_ind)s"""%locals()
+			else:
+				name = """S_%(b_ind)s"""%locals()
+			w=wds.add(name=name)
+			labels_block = []
+			for atom in block:
+				site_index = atom[0]
+				site = "{:03d}".format(site_index)
+				if(atom[1]=='Bi'):
+					label = """Bi(%(site)s)6%(suffix)s"""%locals()
+					labels_block.append(label)
+				if(atom[1]=='Te'):
+					label = """Te(%(site)s)5%(suffix)s"""%locals()
+					labels_block.append(label)
+        		w.addLabels(labels=labels_block,fac=1)
+			b_ind += 1
+        	bw=com.BandWeights('+bweights_mydef')
+		orb_name = labps2[i]
+        	bw.addWeights(wds,"""+bwsum_orb_%(orb_name)s_layer_resolved"""%locals())
+
+
 blocks = identify_blocks()
-print_bwdef(blocks)
+#print_bwdef(blocks,rel=True)
+layer_weights(blocks)
+orbital_layer_weights(blocks)
+orbital_exp_decay(blocks,lambda_0=20)
+orbital_exp_decay(blocks,lambda_0=25)
