f470e4c1563083df421f9d3d075a4f992703fff5
Jorge Facio <facioj@w51lx-facioj.ifw-dresden.de>
2020-06-09 19:27:16 +0200

    example with dos

--- example/example.py
+++ example/example.py
@@ -16,5 +16,7 @@ slab.identify_blocks()
 sites = [41,17,45,15,39,31,7,23,1,21,5,29]
 orbitals = ['6p3/2-3/2','6p3/2+3/2','6p3/2-1/2','6p3/2+1/2','6p1/2-1/2','6p1/2+1/2']
 orbitals = ['5p3/2-3/2','5p3/2+3/2','5p3/2-1/2','5p3/2+1/2','5p1/2-1/2','5p1/2+1/2']
-slab.orbital_dos_exp_decay(sites,orbitals,lambda_0=10./sl.to_angs,dos_folder="./dos")
+orbitals = ['3d+1up']
+sites = [1]
+slab.orbital_dos_exp_decay(sites,orbitals,lambda_0=10./sl.to_angs,dos_folder="./dos",type_file="l")
 
